6a8b54aaaceba37256c9451433dfd0f6e5855ff2
steve <scho93@illinois.edu>
2018-12-08 22:50:16 -0600

    USD conversion working

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,25 +17,24 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 
 public class MainActivity extends AppCompatActivity {
-    Spinner sp;
-    String names[] = {"KRW","EUR"};
-    ArrayAdapter <String> adapter;
+    private static final String TAG = "MP6:Main";
+    private Spinner sp;
+    private String names[] = {"KRW | South-Korean Won", "EUR | Euro", "GBP | British Pound", "CAD | Canadian Dollar",
+                              "AUD | Australian Dollar", "CHF | Swiss Franc", "CNY | Chinese Yuan", "JPY | Japanese Yen", "MXN | Mexican Peso"};
+    private ArrayAdapter <String> adapter;
 
     private static final String API_URL = "https://openexchangerates.org/api/latest.json?app_id=85abb5fdce1c441c916510773248d112";
-    String record = "";
-    TextView display_data;
+    private String record = "";
+    private TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sp = (Spinner)findViewById(R.id.country);
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+        sp = (Spinner)findViewById(R.id.SpinnerChoose);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
         sp.setAdapter(adapter);
         sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -49,8 +47,27 @@ public class MainActivity extends AppCompatActivity {
                     case 1:
                         record = "EUR";
                         break;
-
-
+                    case 2:
+                        record = "GBP";
+                        break;
+                    case 3:
+                        record = "CAD";
+                        break;
+                    case 4:
+                        record = "AUD";
+                        break;
+                    case 5:
+                        record = "CHF";
+                        break;
+                    case 6:
+                        record = "CNY";
+                        break;
+                    case 7:
+                        record = "JPY";
+                        break;
+                    case 8:
+                        record = "MXN";
+                        break;
                 }
             }
 
@@ -59,49 +76,33 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        display_data = (TextView)findViewById(R.id.result);
-
-        Button action = (Button)findViewById(R.id.exchange);
-        action.setOnClickListener(new View.OnClickListener() {
+        result = (TextView)findViewById(R.id.TextViewresult);
+        Button convert = (Button)findViewById(R.id.ButtonConvert);
+        convert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AsyncHttpClient client = new AsyncHttpClient();
                 client.get(API_URL, new AsyncHttpResponseHandler() {
                     @Override
                     public void onSuccess(String response) {
-                        Log.i("CATCHING", "HTTP Success");
+                        Log.i(TAG, "HTTP Success");
 
                         try {
                             JSONObject jsonObj = new JSONObject(response);
                             JSONObject ratesObject = jsonObj.getJSONObject("rates");
 
-                            Double krwRate = ratesObject.getDouble("KRW");
-                            Double eurRate = ratesObject.getDouble("EUR");
-                            Log.i("CATCHING", "KRW: " + krwRate);
-                            Log.i("CATCHING", "EUR: " + eurRate);
+                            Double rate = ratesObject.getDouble(record);
+                            Log.i(TAG, record + rate);
 
 
-                            EditText usedValue = (EditText)findViewById(R.id.editTextUSD);
-                            TextView krwValue = (TextView)findViewById(R.id.textViewKRW);
-                            TextView eurValue = (TextView)findViewById(R.id.textViewEUR);
-                            TextView moneyValue = findViewById(R.id.result);
-                            Double usds = Double.valueOf(usedValue.getText().toString());
-                            Double krws = usds * krwRate;
-                            Double eurs = usds * eurRate;
-                            if(! usedValue.getText().toString().equals("")) {
-                                if (moneyValue.getText().toString().equals("KRW")) {
-                                    moneyValue.setText("KRW: " + String.valueOf(krws));
-                                } else if (moneyValue.getText().toString().equals("EUR")) {
-                                    moneyValue.setText("EUR: " + String.valueOf(eurs));
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Please choose a currency!", Toast.LENGTH_LONG).show();
-                                }
-                                krwValue.setText("KRW: " + String.valueOf(krws));
-                                eurValue.setText("EUR: " + String.valueOf(eurs));
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Please enter a USD value!", Toast.LENGTH_LONG).show();
+                            EditText usdValue = (EditText)findViewById(R.id.editTextUSD);
+                            Double usd = Double.valueOf(usdValue.getText().toString());
+                            Double resultValue = usd * rate;
+                            if(usdValue.getText().toString().equals("")) {
+                                Toast.makeText(getApplicationContext(), "Please enter a USD value", Toast.LENGTH_LONG).show();
                             }
-
+                            result.setTextSize(18);
+                            result.setText(resultValue.toString());
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -116,14 +117,5 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-
-
-
-    }
-
-    public void displayResult(View view) {
-        display_data.setTextSize(18);
-        display_data.setText(record);
-
     }
 }
